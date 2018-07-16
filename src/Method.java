b130990a30a00a5639e33f5442cc9c1b019fae03
Małgorzata Pierzchała <gk.kalinowska@gmail.com>
2018-07-16 10:24:37 +0200

    Zadanie 4.2 - bez klasy Point

--- src/Method.java
+++ src/Method.java
@@ -6,7 +6,7 @@ public class Method {
         if((line1.pointE - line1.pointS) > (line2.pointE - line2.pointS)){
             System.out.println("Odcinek pierwszy jest dłuższy\n" + "Początek: " + line1.pointS + ", " + "Koniec: " + line1.pointE + ", " + "Długość: " + (line1.pointE - line1.pointS));
         } else if (((line1.pointE - line1.pointS) == (line2.pointE - line2.pointS))){
-            System.out.println("Odcinki są równe\n" + "Początek: " + line1.pointS + ", " + "Koniec: " + line1.pointE + ", " + "Długość: " + (line1.pointE - line1.pointS));
+            System.out.println("Odcinki są równe.\n" + "Początek pierwszego odcinka: " + line1.pointS + ", " + "Koniec pierwszego odcinka: " + line1.pointE + ", " + "Długość pierwszego odcinka: " + (line1.pointE - line1.pointS) + "\n" + "Początek drugiego odcinka: " + line2.pointS + ", " + "Koniec drugiego odcinka: " + line2.pointE + ", " + "Długość drugiego odcinka: " + (line2.pointE - line2.pointS));
         } else {
             System.out.println("Odcinek drugi jest dłuższy\n" + "Początek: " + line2.pointS + ", " + "Koniec: " + line2.pointE + ", " + "Długość: " + (line2.pointE - line2.pointS));
 
