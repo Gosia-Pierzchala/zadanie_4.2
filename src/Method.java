4a26f48eb421c4ab77a6f70fe16e54d1cb61833e
Małgorzata Pierzchała <gk.kalinowska@gmail.com>
2018-07-18 21:33:42 +0200

    poprawki

--- src/Method.java
+++ src/Method.java
@@ -1,15 +1,12 @@
 public class Method {
-    Line line1 = new Line(2,4.3);
-    Line line2 = new Line (1.2,5.6);
+    public Line longerLine (Line line1, Line line2){
+        double line1lenght = line1.getP2().getX() - line1.getP1().getX();
+        double line2lenght = line2.getP2().getX() - line2.getP1().getX();
 
-    void findLonger (Line line1, Line line2){
-        if((line1.point.pointE - line1.point.pointS) > (line2.point.pointE - line2.point.pointS)){
-            System.out.println("Odcinek pierwszy jest dłuższy\n" + "Początek: " + line1.point.pointS + ", " + "Koniec: " + line1.point.pointE + ", " + "Długość: " + (line1.point.pointE - line1.point.pointS));
-        } else if (((line1.point.pointE - line1.point.pointS) == (line2.point.pointE - line2.point.pointS))){
-            System.out.println("Odcinki są równe.\n" + "Początek pierwszego odcinka: " + line1.point.pointS + ", " + "Koniec pierwszego odcinka: " + line1.point.pointE + ", " + "Długość pierwszego odcinka: " + (line1.point.pointE - line1.point.pointS) + "\n" + "Początek drugiego odcinka: " + line2.point.pointS + ", " + "Koniec drugiego odcinka: " + line2.point.pointE + ", " + "Długość drugiego odcinka: " + (line2.point.pointE - line2.point.pointS));
+        if(line1lenght > line2lenght){
+            return line1;
         } else {
-            System.out.println("Odcinek drugi jest dłuższy\n" + "Początek: " + line2.point.pointS + ", " + "Koniec: " + line2.point.pointE + ", " + "Długość: " + (line2.point.pointE - line2.point.pointS));
-
+            return line2;
         }
     }
 }
--- src/ScanTest.java
+++ src/ScanTest.java
@@ -3,22 +3,35 @@ import java.util.Scanner;
 public class ScanTest {
     public static void main(String[] args) {
 
-        Line line1 = new Line(2,4.3);
-        Line line2 = new Line (1.2,5.6);
-
         Scanner scan = new Scanner(System.in);
-        System.out.println("Podaj punkt początkowy pierwszego odcinka: ");
-        line1.point.pointS = scan.nextDouble();
-        System.out.println("Podaj punkt końcowy pierwszego odcinka: ");
-        line1.point.pointE = scan.nextDouble();
-        System.out.println("Podaj punkt początkowy drugiego odcinka: ");
-        line2.point.pointS = scan.nextDouble();
-        System.out.println("Podaj punkt końcowy drugiego odcinka: ");
-        line2.point.pointE = scan.nextDouble();
+        System.out.println("Podaj punkt początkowy odcinka pierwszego: ");
+        double a = scan.nextDouble();
+        System.out.println("Podaj punkt końcowy odcinka pierwszego: ");
+        double b = scan.nextDouble();
+        System.out.println("Podaj punkt początkowy odcinka drugiego: ");
+        double c = scan.nextDouble();
+        System.out.println("Podaj punkt końcowy odcinka drugiego: ");
+        double d = scan.nextDouble();
         scan.close();
 
-        Method method = new Method();
-        method.findLonger(line1, line2);
+        Point p1 = new Point (a,10);
+        Point p2 = new Point(b,10);
+        Point p3 = new Point (c,10);
+        Point p4 = new Point(d,10);
 
+        Line line1 = new Line (p1, p2);
+        Line line2 = new Line (p3, p4);
+
+        Method method = new Method();
+        Line longerLine = method.longerLine(line1, line2);
+
+        double line1lenght = line1.getP2().getX() - line1.getP1().getX();
+        double line2lenght = line2.getP2().getX() - line2.getP1().getX();
+
+        if(longerLine == line1){
+            System.out.println("Line1 jest odcinkiem dłuższym" + "\n" + "Początek: " + a + "\n" + "Koniec: " + b + "\n" + "Długość: " + line1lenght);
+        } else {
+            System.out.println("Line2 jest odcinkiem dłuższym" + "\n" + "Początek: " + c + "\n" + "Koniec: " + d + "\n" + "Długość: " + line2lenght);
+        }
     }
 }
